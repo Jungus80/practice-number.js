d6bf47c272274a7da8eb2cd32647cbef3cafb2d6
Jungus80 <106044235+Jungus80@users.noreply.github.com>
2022-07-22 02:31:56 -0500

    Add files via upload

---
+++ var height = 161;.js
@@ -0,0 +1,13 @@
+var  height =  161;
+
+var  height__float = 1.61;
+
+var peso = 63.6;
+
+var altura_redondeado = Math.round(height__float);
+
+var  pesokgrounddown = Math.floor(peso); // redondea hacia abajo
+
+var son_iguales =  Number.MAX_VALUE +1 === Number.MAX_VALUE;
+console.log(son_iguales);
+
